87f47a12ff8a4db6480c4089d8ba73544a2973db
tguissartclub <71717434+tguissartclub@users.noreply.github.com>
2020-10-24 18:16:06 +0900

    Update index2.js

--- index2.js
+++ index2.js
@@ -26,6 +26,7 @@ let a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27
         span.id = "kyodo"+parseInt(a[i]);
         span.class = "hide";
         document.getElementById('kyodoimages').appendChild(span);
+        document.getElementById('image'+a[i]).classList.add("kyodoimage")
       }
     }
   }
