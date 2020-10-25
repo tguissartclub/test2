5281bfb304f7356020943672ca9afa7e03704af7
tguissartclub <71717434+tguissartclub@users.noreply.github.com>
2020-10-25 21:45:47 +0900

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,23 +1,18 @@
 let a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33],
     b = document.getElementById("thumbnails").children,
     j, x, i,
-    resultdata = [], // データを入れるための配列
+    resultdata = [],
     csvfile = new XMLHttpRequest();
-	// csvファイルのパスを指定し、データを開く
     csvfile.open("get", "index.csv", true);
-    csvfile.send(); // HTTPリクエストの発行
-	// csvファイルを読み込んだら処理を実行
+    csvfile.send();
 	csvfile.onload = function(){
-	   let tmp = csvfile.responseText.split("\n"); // 改行を基準にデータを行ごとで配列化
-    // 各行ごとにカンマで区切った文字列の配列データを生成
+	   let tmp = csvfile.responseText.split("\n");
     for(let i = 0; i < tmp.length; i++){
         let tmpROW = tmp[i].split(',');
         if( tmpROW !== '' ){
             resultdata[i] = tmp[i].split(',');
         }
     }
-    // 格納し終えた配列データを元に出力処理
-    console.log(resultdata);
     for (let i = 0; i < a.length; i++) {
       document.getElementById("thumbnails").children[i].style.backgroundImage = "url(thumbnail/"+a[i]+".jpg)";
       document.getElementById("thumbnails").children[i].setAttribute("onclick","loadimage("+a[i]+","+(i+1)+")");
